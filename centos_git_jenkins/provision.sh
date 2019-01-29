8b368673394e2c1ee2a0f7c2b92bfcc87b30ba06
Damien Lust <damienlust@Damiens-MacBook-Pro-831.local>
2019-01-29 08:46:22 +0100

    update

---
+++ centos_git_jenkins/provision.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+yum install -y git
+yum install -y java
+
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+yum install -y jenkins
+yum install -y docker
+systemctl start jenkins
